38ead4229ac2ed9f7ed864cf41710f19d8e6fc3d
Vincenzo- <viulenzgg@gmail.com>
2023-10-03 15:31:13 +0200

    installed Automatic Class Sorting with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className="bg-blue-600 text-3xl font-bold text-green-600">Hello world!</h1>
     </>
   );
 }
