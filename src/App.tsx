2ab0a19591d877af888e5c361a945758620f2b92
Vincenzo- <viulenzgg@gmail.com>
2023-10-06 16:15:06 +0200

    added hook to detect scroll direction

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BottomNav, TopBar } from "./components";
+import { useScrollDirection } from "./hooks/useScrollDirection";
 
 function App() {
+  const [scrollDir] = useScrollDirection();
+  console.log(scrollDir);
+
   return (
     <>
       <div className="container">
@@ -9,7 +13,9 @@ function App() {
         <div className="flex min-h-[48px] items-center justify-center text-[15px] font-light text-[#1D9BF0]">Show 46 posts</div>
         <div className="h-[1px] bg-[#2F3336]"></div>
 
-        <BottomNav />
+        <div className={`${scrollDir === "u" ? "opacity-1" : "opacity-40"} min-[500px]:hidden`}>
+          <BottomNav />
+        </div>
       </div>
     </>
   );
--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -5,7 +5,7 @@ import MailIcon from "../../assets/icons/MailIcon.svg";
 
 export const BottomNav: React.FC = () => {
   return (
-    <footer className="fixed bottom-0 left-0 right-0">
+    <footer className="fixed bottom-0 left-0 right-0 bg-black">
       <div className="h-[1px] bg-[#2F3336]"></div>
       <nav className="grid h-14 grid-cols-4 items-center justify-items-center">
         <a href="#">
